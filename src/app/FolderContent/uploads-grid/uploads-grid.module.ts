4cea03afc77a087de6a168c19e3c02cecbc2c03b
Dvir Reich <38475641+dvireich@users.noreply.github.com>
2018-10-11 16:18:51 +0300

    Added ngDraggable to the upload grid

--- src/app/FolderContent/uploads-grid/uploads-grid.module.ts
+++ src/app/FolderContent/uploads-grid/uploads-grid.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UploadsGrid } from './upload-progress-container.component/uploads-grid.component';
 import { UploadProgressRow } from './upload-progress-container.component/upload-progress-row.component/upload-progress-row.component';
+import { SharedModule } from '../../Common/common.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   declarations: [
     UploadProgressRow,
