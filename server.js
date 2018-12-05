1945d22994ee4c9f4a2fe7331934dff756d41cb1
Dvir Reich <38475641+dvireich@users.noreply.github.com>
2018-12-06 01:23:13 +0200

    server.js

--- server.js
+++ server.js
@@ -8,11 +8,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, "dist", "my-project")));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname, 'my-project/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/my-project/index.html'));
   });
-// app.get('/*', function(req,res) {
-//     res.sendFile(path.join(__dirname, 'dist/my-project/index.html'));
-//   });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
